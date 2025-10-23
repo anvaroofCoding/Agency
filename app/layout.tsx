c6ba266ffc87a6a65a0511d429345e2e7036271a
Islom <islom@Isloms-MacBook-Air.local>
2025-10-23 09:50:41 +0500

    mains

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-	title: 'v0 App',
-	description: 'Created with v0',
-	generator: 'v0.app',
+	title: 'Islom Anvar',
+	description: 'Frontend developer',
 }
 
 export default function RootLayout({
